3d613c03250f1f4c7c859dd77cd4671dc6adf1b0
Satoshi Nagayasu <snaga@uptime.jp>
2013-11-09 23:09:15 -0500

    Fix dd script and update log file.

--- bin/dd.sh
+++ bin/dd.sh
@@ -2,30 +2,32 @@
 
 # Copyright(c) 2013 Uptime Technologies, LLC.
 
-echo /home/dd.out
-dd if=/dev/zero of=/home/dd.out bs=8192 count=100000
-dd if=/dev/zero of=/home/dd.out bs=8192 count=100000
-dd if=/dev/zero of=/home/dd.out bs=8192 count=100000
+cat /dev/null > dd.log
 
-echo /var/tmp/dd.out
-dd if=/dev/zero of=/var/tmp/dd.out bs=8192 count=100000
-dd if=/dev/zero of=/var/tmp/dd.out bs=8192 count=100000
-dd if=/dev/zero of=/var/tmp/dd.out bs=8192 count=100000
+echo /home/dd.out >> dd.log
+dd if=/dev/zero of=/home/dd.out bs=8192 count=100000 >> dd.log 2>&1
+dd if=/dev/zero of=/home/dd.out bs=8192 count=100000 >> dd.log 2>&1
+dd if=/dev/zero of=/home/dd.out bs=8192 count=100000 >> dd.log 2>&1
 
-echo /disk/disk2/dd.out
-dd if=/dev/zero of=/disk/disk2/dd.out bs=8192 count=100000
-dd if=/dev/zero of=/disk/disk2/dd.out bs=8192 count=100000
-dd if=/dev/zero of=/disk/disk2/dd.out bs=8192 count=100000
+echo /var/tmp/dd.out >> dd.log
+dd if=/dev/zero of=/var/tmp/dd.out bs=8192 count=100000 >> dd.log 2>&1
+dd if=/dev/zero of=/var/tmp/dd.out bs=8192 count=100000 >> dd.log 2>&1
+dd if=/dev/zero of=/var/tmp/dd.out bs=8192 count=100000 >> dd.log 2>&1
 
-echo /disk/disk3/dd.out
-dd if=/dev/zero of=/disk/disk3/dd.out bs=8192 count=100000
-dd if=/dev/zero of=/disk/disk3/dd.out bs=8192 count=100000
-dd if=/dev/zero of=/disk/disk3/dd.out bs=8192 count=100000
+echo /disk/disk2/dd.out >> dd.log
+dd if=/dev/zero of=/disk/disk2/dd.out bs=8192 count=100000 >> dd.log 2>&1
+dd if=/dev/zero of=/disk/disk2/dd.out bs=8192 count=100000 >> dd.log 2>&1
+dd if=/dev/zero of=/disk/disk2/dd.out bs=8192 count=100000 >> dd.log 2>&1
 
-echo /disk/disk4/dd.out
-dd if=/dev/zero of=/disk/disk4/dd.out bs=8192 count=100000
-dd if=/dev/zero of=/disk/disk4/dd.out bs=8192 count=100000
-dd if=/dev/zero of=/disk/disk4/dd.out bs=8192 count=100000
+echo /disk/disk3/dd.out >> dd.log
+dd if=/dev/zero of=/disk/disk3/dd.out bs=8192 count=100000 >> dd.log 2>&1
+dd if=/dev/zero of=/disk/disk3/dd.out bs=8192 count=100000 >> dd.log 2>&1
+dd if=/dev/zero of=/disk/disk3/dd.out bs=8192 count=100000 >> dd.log 2>&1
+
+echo /disk/disk4/dd.out >> dd.log
+dd if=/dev/zero of=/disk/disk4/dd.out bs=8192 count=100000 >> dd.log 2>&1
+dd if=/dev/zero of=/disk/disk4/dd.out bs=8192 count=100000 >> dd.log 2>&1
+dd if=/dev/zero of=/disk/disk4/dd.out bs=8192 count=100000 >> dd.log 2>&1
 
 rm -f /home/dd.out /var/tmp/dd.out /disk/disk2/dd.out /disk/disk3/dd.out /disk/disk4/dd.out
 
