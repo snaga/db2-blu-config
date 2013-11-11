f715723f05cec57a3c9761830e65a3937c37840f
Satoshi Nagayasu <snaga@uptime.jp>
2013-11-11 02:38:13 -0500

    Add SQL scripts for DBT-3 schema definition for DB2.

---
+++ dbt3/dbt3-db2-describe-tables.sql
@@ -0,0 +1,15 @@
+--
+-- This file is released under the terms of the Artistic License.
+-- Please see the file LICENSE, included in this package, for details.
+--
+-- Copyright (C) 2002-2008 Mark Wong & Open Source Development Labss, Inc.
+-- Copyright (C) 2013 Uptime Technologies, LLC.
+--
+DESCRIBE TABLE supplier;
+DESCRIBE TABLE part;
+DESCRIBE TABLE partsupp;
+DESCRIBE TABLE customer;
+DESCRIBE TABLE orders;
+DESCRIBE TABLE lineitem;
+DESCRIBE TABLE nation;
+DESCRIBE TABLE region;
