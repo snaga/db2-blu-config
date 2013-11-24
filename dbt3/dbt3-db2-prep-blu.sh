0b81c618288a4c487bd00098e4863f1f2f20e493
Satoshi Nagayasu <snaga@uptime.jp>
2013-11-24 18:10:04 +0900

    Fix preparation scripts for both intra-parallel and BLU.

---
+++ dbt3/dbt3-db2-prep-blu.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Copyright(c) 2013 Uptime Technologies, LLC.
+
+DBNAME=$1
+PARTITION=$2
+
+if [ "$PARTITION" = "1" ]; then
+    echo -n ""
+elif [ "$PARTITION" = "2" ]; then
+    echo -n ""
+elif [ "$PARTITION" = "4" ]; then
+    echo -n ""
+else
+    echo "Usage: $0 <DBNAME> <NUM_OF_PARTITION>"
+    exit
+fi;
+
+cat /dev/null > prep_dbt3.log
+
+db2 "connect to ${DBNAME}${PARTITION}" | tee -a prep_dbt3.log
+db2 -tvf ../dbt3/dbt3-db2-drop-tables.sql | tee -a prep_dbt3.log
+db2 -tvf ../dbt3/dbt3-db2-create-tables.sql | tee -a prep_dbt3.log
+#db2 -tvf ../dbt3/dbt3-db2-create-indexes.sql | tee -a prep_dbt3.log
+db2 -tvf ../dbt3/dbt3-db2-describe-tables.sql | tee -a prep_dbt3.log
