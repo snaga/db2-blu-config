87436f10db347bd6473f2920445bc7f692300d80
Satoshi Nagayasu <snaga@uptime.jp>
2013-11-30 12:19:13 +0900

    Fix COPY command syntax in the data loading script.

--- dbt3/dbt3-pgsql-load-data.sql
+++ dbt3/dbt3-pgsql-load-data.sql
@@ -5,11 +5,11 @@
 -- Copyright (C) 2002-2008 Mark Wong & Open Source Development Labss, Inc.
 -- Copyright (C) 2013 Uptime Technologies, LLC.
 --
-\COPY supplier FROM '/tmp/data/supplier.csv' ( FORMAT CSV )
-\COPY part FROM '/tmp/data/part.csv' ( FORMAT CSV )
-\COPY partsupp FROM '/tmp/data/partsupp.csv' ( FORMAT CSV )
-\COPY customer FROM '/tmp/data/customer.csv' ( FORMAT CSV )
-\COPY orders FROM '/tmp/data/orders.csv' ( FORMAT CSV )
-\COPY lineitem FROM '/tmp/data/lineitem.csv' ( FORMAT CSV )
-\COPY nation FROM '/tmp/data/nation.csv' ( FORMAT CSV )
-\COPY region FROM '/tmp/data/region.csv' ( FORMAT CSV )
+\COPY supplier FROM '/tmp/data/supplier.csv' WITH ( FORMAT CSV );
+\COPY part FROM '/tmp/data/part.csv' WITH ( FORMAT CSV );
+\COPY partsupp FROM '/tmp/data/partsupp.csv' WITH ( FORMAT CSV );
+\COPY customer FROM '/tmp/data/customer.csv' WITH ( FORMAT CSV );
+\COPY orders FROM '/tmp/data/orders.csv' WITH ( FORMAT CSV );
+\COPY lineitem FROM '/tmp/data/lineitem.csv' WITH ( FORMAT CSV );
+\COPY nation FROM '/tmp/data/nation.csv' WITH ( FORMAT CSV );
+\COPY region FROM '/tmp/data/region.csv' WITH ( FORMAT CSV );
